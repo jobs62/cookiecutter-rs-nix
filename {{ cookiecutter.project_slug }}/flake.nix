9fd1b26c74af34a50fc075f4eb137a47a8431e42
Ch.-David Blot <charles-david.blot@orange.com>
2023-11-10 13:58:22 +0100

    first commit

---
+++ {{ cookiecutter.project_slug }}/flake.nix
@@ -0,0 +1,59 @@
+{
+  description = "{{ cookiecutter.description }}";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    crane = {
+        url = "github:ipetkov/crane";
+        inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.rust-analyzer-src.follows = "";
+    };
+
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, crane, fenix, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+
+        inherit (pkgs) lib;
+
+        craneLib = crane.lib.${system};
+        src = craneLib.cleanCargoSource (craneLib.path ./.);
+
+        commonArgs = {
+          inherit src;
+          strictDeps = true;
+        };
+
+        craneLibLlvmTools = craneLib.overrideToolchain
+          (fenix.packages.${system}.complete.withComponents [
+            "cargo"
+            "llvm-tools"
+            "rustc"
+          ]);
+
+        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+
+        {{ cookiecutter.project_slug }} = craneLib.buildPackage (commonArgs // {
+            inherit cargoArtifacts;
+        });
+      in {
+        packages = {
+            default = {{ cookiecutter.project_slug }};
+        };
+
+        apps.default = flake-utils.lib.mkApp {
+            drv = {{ cookiecutter.project_slug }};
+        };
+      }
+    );
+}
