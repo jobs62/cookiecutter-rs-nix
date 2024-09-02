7007d28c5f8c8cde6668272f9957221139b77652
jobs62 <droper@analphabet.me>
2024-09-02 11:12:56 +0200

    Update flake.nix

--- {{ cookiecutter.project_slug }}/flake.nix
+++ {{ cookiecutter.project_slug }}/flake.nix
@@ -25,7 +25,7 @@
 
         inherit (pkgs) lib;
 
-        craneLib = crane.lib.${system};
+        craneLib = crane.mkLib pkgs;
         src = craneLib.cleanCargoSource (craneLib.path ./.);
 
         commonArgs = {
